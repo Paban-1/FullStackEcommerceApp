def62132cbc1f36a82d200b27cfc75faf40a8ce3
Paban-1 <rpaban89@gmail.com>
2026-03-16 09:18:43 +0530

    Created UserModel

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log("PORT is runing on 3000");
+
+})
